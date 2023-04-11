03d490064cbc2aa357eab3590521052983e4c67f
Apollo <88677238+ApolloDraco@users.noreply.github.com>
2023-04-11 17:18:18 -0400

    Update Polygon.java

--- Polygon.java
+++ Polygon.java
@@ -3,21 +3,34 @@
 */
 
 import java.util.Scanner;
+
 public class Polygon {
   public static void main(String args[]) {
-     Scanner sc=new Scanner(System.in);
+     Scanner sc = new Scanner(System.in);
      
      //Enter number of sides
-     System.out.print("Enter the number of sides in polygon:");
-     double n=sc.nextDouble();
+     System.out.print("Enter the number of sides in polygon: ");
+     int numberOfSides = 0;
+     try {
+        numberOfSides = sc.nextInt();
+     } catch (Exception e) {
+        System.out.println("Invalid input. Please enter a valid integer.");
+        System.exit(0);
+     }
      
      //Enter length of sides 
-     System.out.print("Enter the length of sides in polygon:");
-     double ln=sc.nextDouble();
+     System.out.print("Enter the length of sides in polygon: ");
+     double lengthOfSide = 0;
+     try {
+        lengthOfSide = sc.nextDouble();
+     } catch (Exception e) {
+        System.out.println("Invalid input. Please enter a valid number.");
+        System.exit(0);
+     }
     
-    double angle= Math.toRadians(180/n);
-    angle=Math.tan(angle);
-    double area=((ln*ln*n)/(4*angle));
-    System.out.print("Area of polygon is:"+area);
+    double angle = Math.toRadians(180/numberOfSides);
+    angle = Math.tan(angle);
+    double area = ((lengthOfSide * lengthOfSide * numberOfSides)/(4 * angle));
+    System.out.print("Area of polygon is: " + area);
   }
 }
